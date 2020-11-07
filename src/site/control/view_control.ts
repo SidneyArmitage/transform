cfcdb5b0c15314d8137c70c1fc223aacc879a54c
SidneyArmitage <sichy@live.com.au>
2020-11-07 11:00:07 +1100

    added multiplier to scaling for a more user friendly feeling

--- src/site/control/view_control.ts
+++ src/site/control/view_control.ts
@@ -92,7 +92,7 @@ export class View_control {
   }
 
   public on_wheel (event: WheelEvent) {
-    this.scale = Math.max(this.scale + event.deltaY * -0.01, 0.001);
+    this.scale = Math.max(this.scale + this.scale * event.deltaY * -0.01, 0.001);
     this.size = {
       x: 1 / this.scale,
       y: 1 / this.scale,
